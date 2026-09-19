2c7b1a8d2fa2725f33f9aa6e905672ce56f0d2ea
Gil-gil-glitch <gilsocojp@gmail.com>
2026-09-20 04:09:52 +0900

    Corrected entry point for head node

--- src/nova_arm_driver/setup.py
+++ src/nova_arm_driver/setup.py
@@ -31,7 +31,7 @@ setup(
             'qr_trigger = nova_arm_driver.qr_trigger:main',
             'HeadDriver = nova_arm_driver.head_driver:main',
             'presenter_coordinator = nova_arm_driver.presentor_coordinator:main',
-            'HeadNode = nova_arm_driver.head_node:main',
+            'HeadNode = nova_arm_driver.HeadNode:main',
         ],
     },
 )
